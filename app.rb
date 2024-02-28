87ec14021700deda4e5f841bbab644d4980b2e9b
Jennifer Rahman <81711306+jb520@users.noreply.github.com>
2024-02-28 22:44:39 +0000

    add square and square root results

--- app.rb
+++ app.rb
@@ -20,3 +20,19 @@ end
 get("/random/new") do
   erb(:random)
 end
+
+#results pages
+
+get("/square/results") do
+  @num = params.fetch("number").to_f
+  @result = @num ** 2
+
+  erb(:calc_square)
+end
+
+get("/square_root/results") do
+  @num = params.fetch("number").to_f
+  @result = Math.sqrt(@num)
+
+  erb(:calc_square_root)
+end
